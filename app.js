6e2362c809c7abc5d5654344bf7edf14339b6a42
jshprintz <jshprintz@gmail.com>
2023-07-12 14:13:46 -0700

    randomly drawn squares

--- app.js
+++ app.js
@@ -51,3 +51,69 @@ circle.x = 200;
 circle.y = 200;
 app.stage.addChild(circle);
 
+const line = new Graphics();
+line
+  .lineStyle(4, 0xefefef, 1)
+  .moveTo(0, 0)
+  .lineTo(0, 100)
+  .lineTo(100, 100)
+  .lineTo(100, 0);
+
+line.x = 10;
+line.y = 10;
+
+app.stage.addChild(line);
+
+const torus = new Graphics();
+torus
+  .beginFill(0x66ccff)
+  .lineStyle(4, 0xefefef, 1)
+  .drawTorus(100, 300, 80, 100, 0, Math.PI / 2)
+  .endFill();
+
+torus.x = 20;
+torus.y = 20;
+app.stage.addChild(torus);
+
+const star = new Graphics();
+star
+  .beginFill(0x66ccff)
+  .lineStyle(4, 0xefefef, 1)
+  .drawStar(300, 300, 6, 50)
+  .endFill();
+
+star.x = 20;
+star.y = 20;
+app.stage.addChild(star);
+
+const style = new PIXI.TextStyle({
+  fontFamily: "Arial",
+  fontSize: 36,
+  fill: "white",
+  stroke: "#ff3300",
+  strokeThickness: 4,
+  dropShadow: true,
+  dropShadowColor: "#000000",
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+});
+
+const myText = new PIXI.Text("Hello World", style);
+myText.x = 200;
+myText.y = 200;
+app.stage.addChild(myText);
+
+
+app.ticker.add((delta) => loop(delta));
+
+function loop(delta) {  
+  const rectangle = new Graphics();
+  rectangle
+    .beginFill(0x66ccff)
+    .lineStyle(4, 0xefefef, 1)
+    .drawRect(Math.random() * app.screen.width, Math.random() * app.screen.height, 10, 10)
+    .endFill();
+  // Add the rectangle to the stage
+  app.stage.addChild(rectangle);
+}
